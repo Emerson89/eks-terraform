ec361990af2f7e10ea7f33a21b413d682f90b0b4
Emerson89 <emerson.cesario50@gmail.com>
2023-08-12 12:45:03 -0300

    fix: ajust name cluster

--- master.tf
+++ master.tf
@@ -9,7 +9,7 @@ data "aws_eks_cluster_auth" "this" {
 resource "aws_cloudwatch_log_group" "this" {
   # The log group name format is /aws/eks/<cluster-name>/cluster
   # Reference: https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html
-  name              = "/aws/eks/${var.cluster_name}-${var.environment}/cluster"
+  name              = "/aws/eks/${var.cluster_name}/cluster"
   retention_in_days = 7
 }
 
